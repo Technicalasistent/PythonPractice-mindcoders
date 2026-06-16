8efaa1531b8c16bae6569c143406e28f9d46705f
unknown <pratikshitasthana@gmail.com>
2026-06-16 17:42:59 +0530

    matplotlip

---
+++ matplotlib123.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+
+# months=['Jan','Feb','Mar','Apr','May','June','July','Aug','Sep','Oct','Nov','Dec']
+# sales=[45,52,48,61,58,72,69,75,68,82,90,95]
+
+# plt.figure(figsize=(12,5))
+# plt.plot(months,sales,marker='+',color='steelblue',linewidth=2,markersize=8)
+# plt.fill_between(months,sales,alpha=0.15,color='steelblue')
+# plt.title('monthly sales 2024(Rs. Thousands)',fontsize=14,fontweight='bold')
+# plt.xlabel('months')
+# plt.ylabel('sales (Rs. K)')
+# plt.grid(True,alpha=0.3)
+# plt.tight_layout()
+# plt.show()
+
+
+#bar graph
+# cities=['Bhopal','Indore','Jabalpur','Gwalior','Ujjain']
+# students=[1200,2800,980,850,650]
+# colors=['#2196F3','#4CAF50','#FF9800','#9C27B0','#F44336']
+
+# # Bar Chart
+# plt.figure(figsize=(9,5))
+# bars=plt.bar(cities,students,color=colors,edgecolor='white',linewidth='1.5')
+# plt.title('Students enrolled per cities')
+# plt.ylabel('Number of studdents')
+# for bar,val in zip(bars,students):
+#     plt.text(bar.get_x()+bar.get_width()/2,val+30,str(val),ha='center',fontweight='bold')
+# plt.tight_layout()
+# plt.show()
+
+import numpy as np
+#scatter plot
+study_hrs=np.random.uniform(2,10,50)
+marks=study_hrs*7+np.random.normal(0,8,50)
+marks=np.clip(marks,30,100)
+
+plt.figure(figsize=(8,5))
+plt.scatter(study_hrs,marks,c=marks,cmap='RdYlGn',s=100,alpha=0.8)
+plt.colorbar(label='Marks')
+plt.title('Study hours vs exam marks')
+plt.xlabel('Study hours/day')
+plt.ylabel('Exam Marks')
+plt.show()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,11 +1,1 @@
-ones=np.ones((2,5))
-# print(ones)
-
-# rng=np.arange(0,50,5)
-# print(rng)
-
-# lin=np.linspace(0,1,11)
-# print(lin)
-
-# random=np.random.randint(40100,(5,3))
-# print(random)
+Name
